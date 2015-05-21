769cb2f8602ce3cb48e62c11225d3f9393331083
tim shores <lee.shoe@gmail.com>
2015-05-20 20:45:03 -0700

    Directory housekeeping

--- quiz1.R
+++
@@ -1,5 +0,0 @@
-quiz question 3
-gdp <- read.csv("gdp.csv", nrows=190, skip=4)
-m1 <- merge(gdp, edu, by.x = "X", by.y = "CountryCode") # produces 189 matches
-m3$"High income: OECD"[2]
-m3$"High income: nonOECD"[2]
