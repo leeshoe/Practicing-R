81a6721cc0594ec76f3bac55c68b5b24adfce22d
tim shores <lee.shoe@gmail.com>
2015-06-13 10:34:49 -0700

    Project 2 gitrdone

---
+++ Exploratory Data Analysis Projects/exdata_data_NEI_data/plot1.R
@@ -0,0 +1,8 @@
+# Step 1: just noodlin' around, exploring my new DFs
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("Exploratory Data Analysis Projects/exdata_data_NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("Exploratory Data Analysis Projects/exdata_data_NEI_data/Source_Classification_Code.rds")
+# object_size(NEI)
+# 680 MB
+# object_size(SCC)
+# 3.74 MB
