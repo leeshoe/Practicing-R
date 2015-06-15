d490408f671dea02bbd35ecf9e6ba0424ecbe445
tim shores <lee.shoe@gmail.com>
2015-06-14 20:55:26 -0700

    finally doing some spelunking on NEI dataframe

--- Exploratory Data Analysis Projects/exdata_data_NEI_data/plot1.R
+++ Exploratory Data Analysis Projects/exdata_data_NEI_data/plot1.R
@@ -6,3 +6,8 @@ SCC <- readRDS("Exploratory Data Analysis Projects/exdata_data_NEI_data/Source_C
 # 680 MB
 # object_size(SCC)
 # 3.74 MB
+
+plot1 <- NEI[c("Emissions", "year")]
+plot1$year <- as.factor(plot1$year)
+# Next step: split, use lapply to sum the Emissions, then there's 4 to plot?
+plot1 <- split(plot1, as.factor(plot1$year))
